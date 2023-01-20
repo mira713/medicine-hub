9c842b2135fd9158797b34f319bf5369bae83d23
pallavi jain <pallavijainy0612@gmail.com>
2023-01-20 14:25:47 +0530

    add home page finally

--- medical-hub/src/App.js
+++ medical-hub/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Combo from './component/product/combo';
+import Disease from './component/product/Disease';
+import Medicine from './component/product/Medicine';
+import LightningDeals from './component/product/Product';
+import Navbar from './component/Navbar/Navbar';
+import Category from "../src/component/Navbar/Category.jsx"
+import SearchNav from "../src/component/Navbar/searchnav.jsx"
+import Home from './Pages/Home';
+import Footer from "../src/Footer/Footer.jsx";
+import HomeFooter from './Footer/HomeFooter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     <SearchNav/>
+     <Category/>
+     <Home/>
+   <LightningDeals/>
+   <Combo/>
+   <Medicine/>
+   <Disease/>
+   <HomeFooter/>
+     <Footer/>
     </div>
   );
 }
