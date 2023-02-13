a2749759fe73fd3d37f70e5c56a868dac67de4ed
sumeet.rana <sumeet.rana@innovaccer.com>
2023-02-14 01:23:34 +0530

    code-review

--- medical-hub/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-
-import { legacy_createStore,applyMiddleware } from 'redux';
-import thunk from "redux-thunk";
-import ProductReducer from './ProductReducer';
-
-
-
-export const store = legacy_createStore(ProductReducer,applyMiddleware(thunk))
